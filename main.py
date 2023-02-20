965840b0a090d7942250845521396588cd4fdcfd
AleksandraPetrovska <125836561+AleksandraPetrovska@users.noreply.github.com>
2023-02-20 02:05:01 +0000

    221RDB253 3.grupa Aleksandra Petrovska

--- main.py
+++ main.py
@@ -14,23 +14,36 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next,i+1))
-            # Process opening bracket, write your code here
-            #pass
         if next in ")]}":
-            # Process closing bracket, write your code here
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1]. char, next):
                 return i+1
             opening_brackets_stack.pop()
-            
+
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
     return "Success"
 
 
 def main():
-    text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    print(mismatch)
+    check = input()
+    if "F" in check:
+        text = input()
+        with open(text, "r", encoding="ISO-8859-1") as file:
+            module = file.read()
+        mismatch = find_mismatch(module)
+        if mismatch == "Success" :
+            print("Success")
+        else:
+            print(mismatch)
+    elif "I" in check:
+        text = input()
+        mismatch = find_mismatch(text)
+        if mismatch == "Success" :
+            print("Success")
+        else:
+            print(mismatch)
+    else:
+        mismatch = find_mismatch(check)
+        print(mismatch)
 
 main()
